fe72683021d1f7557ebb12fb73cc1c1103801f23
夜航星星亮 <1132842293@qq.com>
2024-09-22 23:46:47 +0800

    自定义装饰器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Request, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission } from './custom.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('aaa')
-  @SetMetadata('require-login', true)
-  @SetMetadata('require-permission', ['ddd'])
+  @RequireLogin()
+  @RequirePermission('ddd')
   aaaa(@Request() req) {
     console.log('rrrr', req.user);
     return 'aaa';
