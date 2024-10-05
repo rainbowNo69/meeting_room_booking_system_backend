3a91f00c327a3b7bdf98c909b3b885c5f5779c32
夜航星星亮 <1132842293@qq.com>
2024-10-06 00:53:41 +0800

    Response格式化Interceptor

---
+++ src/format-response.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class FormatResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse<Response>();
+    //抛出的异常还是由内置的 Exception Filter 来处理,不在此被处理
+    return next.handle().pipe(
+      map((data) => {
+        console.log('111', {
+          code: response.statusCode,
+          message: 'success',
+          data,
+        });
+        return {
+          code: response.statusCode,
+          message: 'success',
+          data,
+        };
+      }),
+    );
+  }
+}
