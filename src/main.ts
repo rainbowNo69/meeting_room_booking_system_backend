65e0c5bb993f195fa3a4046cfed1fbcebdefe96b
夜航星星亮 <1132842293@qq.com>
2024-10-07 22:09:14 +0800

    feat 修改密码、修改信息接口

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';//eslint-disable-line
+import 'tsconfig-paths/register';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
